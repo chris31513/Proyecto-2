4b6a72ee5ae05e24a08540142e1182facc048685
Christian <chris3151@ciencias.unam.mx>
2018-10-01 20:51:53 -0500

    Minero con sub-carpetas listo

--- src/Steve.py
+++ src/Steve.py
@@ -3,18 +3,24 @@ import eyed3
 eyed3.log.setLevel("ERROR")
 class Steve(object):
         def __init__(self):
+                self.filepath = []
+                self.carp = []
                 self.artist = []
                 self.albums = []
                 self.songs = []
         def get_mp3(self):
                 newpath = os.path.expanduser('~/Music')
                 for root, dirs, files in os.walk(newpath):
-                        return files
+                        for file in files:
+                                self.filepath.extend([os.path.join(root,file)])
         def get_mp3_tags(self):
-                for i in self.get_mp3():
-                        audio = eyed3.load(os.path.expanduser('~/Music')+'/'+i)
+                for i in self.filepath:
+                        audio = eyed3.load(i)
                         self.artist.append(audio.tag.artist)
                         self.albums.append(audio.tag.album)
                         self.songs.append(audio.tag.title)
+
+                        
+
 
 
